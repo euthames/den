f30e708a6d73c82a3296a4d5eed25cf5850feb6c
kuvet <kuvet@macbook.com>
2019-11-25 22:46:26 +0300

    New functions are added

--- dataStructures/linkedList.cpp
+++ dataStructures/linkedList.cpp
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,42 +8,52 @@ struct n{
 
 typedef n node;
 
-// print given linked kist
-int printLinkedList(node * r){
+// print given linked list 
+void printLinkedList(node * r){
 	while(r != NULL){
 		printf(" %d \n",r -> data);
 		r = r -> next;
 	}
 	printf("END of Linked List\n");
-	return 0;
 }
 
-int addNode(node ** r_ptr, int val){
-	
+int addNodeToBegining(node ** r_ptr, int val){	
 	// add first Node if not exist
 	if(*r_ptr==NULL){
 		*r_ptr = (node*)malloc(sizeof(node));
 		(*r_ptr) -> next = NULL;
 		(*r_ptr) -> data = val;
 	}
-	else{// add node to the end of the list
-		// iterate to the end of the list 	
-		node * iter = *r_ptr;
-		while(iter -> next != NULL)
-			iter = iter -> next;
-		// create and add node to the end of the list and make sure ended with NULL next ptr of the new node 
-		iter -> next = (node*)malloc(sizeof(node));
-		iter -> next -> next = NULL;
-		iter -> next -> data = val;
+	else{
+		node * temp = (node*)malloc(sizeof(node));
+		temp -> next = *r_ptr;
+		temp -> data = val;
+		*r_ptr = temp;
 	}
+	return 0;
+}
+
+int addNodeToEnd(node * iter, int val){
+	if(iter == NULL)
+		return 1;
+	while(iter -> next != NULL)
+		iter = iter -> next;
+
+	iter -> next = (node *)malloc(sizeof(node));
+	iter -> next -> data = val;
+	iter -> next -> next = NULL;
+
 	return 0;
 }
 
 int main(){
 	node* root = NULL;
-	
+
 	for(int i=1;i<=10;i++)
-		addNode(&root,i*10);
+		addNodeToBegining(&root,i*100);
+
+	for(int i=1;i<=10;i++)
+		addNodeToEnd(root,i*10);
 
 	printLinkedList(root);
 
